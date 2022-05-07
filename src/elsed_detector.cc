43cf54613587d1352f39420077aef07c8afb4cf0
HyeonJaeGil <now9728@gmail.com>
2022-05-07 14:47:34 +0900

    implement imshow function

--- src/elsed_detector.cc
+++ src/elsed_detector.cc
@@ -1,12 +1,19 @@
 #include "elsed_detector.h"
 
 
-void ElsedDetector::DetectLineFeature(cv::Mat& img_in)
+void ElsedDetector::DetectLineFeature(cv::Mat img_in)
 {
     orig_img = img_in.clone();
     segs = elsed.detect(img_in);
     std::cout << "ELSED detected: " << segs.size() << " (large) segments" << std::endl;
 
-    drawSegments(img_in, segs, CV_RGB(0, 255, 0), 2);
     // cv::imshow("ELSED long", img_in);
+}
+
+void ElsedDetector::ShowDetectedImage(char* window_title)
+{
+    detected_img = orig_img.clone();
+    drawSegments(detected_img, segs, CV_RGB(0, 255, 0), 2);
+    cv::imshow(window_title, detected_img);
+
 }
